a0ad10146e2d28653d29c2aefc71b1893148594c
jython234 <jython234@users.noreply.github.com>
2017-03-16 17:20:29 -0500

    Add OPERATION_UPDATE_CLIENT_EXECUTABLE and bump API version to 3-2.

--- src/main/java/io/github/jython234/nectar/server/struct/operation/OperationID.java
+++ src/main/java/io/github/jython234/nectar/server/struct/operation/OperationID.java
@@ -1,11 +1,14 @@
 package io.github.jython234.nectar.server.struct.operation;
 
 /**
- * Created by jython234 on 3/14/17.
+ * Represents the identifier for a specific operation.
+ *
+ * @author jython234
  */
 public enum OperationID {
     OPERATION_DO_UPDATE(0),
     OPERATION_INSTALL_PACKAGE(1),
+    OPERATION_UPDATE_CLIENT_EXECUTABLE(2),
     OPERATION_SET_TIMEZONE(20),
     OPERATION_DEPLOY_SCRIPT(30),
     OPERATION_DO_SHUTDOWN(40),
