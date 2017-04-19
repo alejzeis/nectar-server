300cf129f37cb143c29f7dae5bb58884f5f12d45
jython234 <jython234@users.noreply.github.com>
2017-04-18 20:36:17 -0500

    Update gen-packages.sh to include build number

--- dist/gen-packages.sh
+++ dist/gen-packages.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
+BUILD_NUMBER=0
+
+if [ $# == 0 ]; then
+    BUILD_NUMBER=1
+else
+    BUILD_NUMBER=$1
+fi
+
 # Build Deb
-fpm -s dir -t deb -a all -n nectar-server -v 0.2.1 --after-install nectar-server-install.sh ../target/Nectar-Server-0.2.1-SNAPSHOT.jar=/usr/lib/nectar-server/Nectar-Server.jar nectar-server=/usr/bin/nectar-server nectar-server.service=/usr/lib/systemd/system/nectar-server.service ../genkeys.sh=/etc/nectar-server/genkeys.sh
+fpm -s dir -t deb -a all -n nectar-server -v 0.2.1 --iteration $BUILD_NUMBER --after-install nectar-server-install.sh ../target/Nectar-Server-0.2.1-SNAPSHOT.jar=/usr/lib/nectar-server/Nectar-Server.jar nectar-server=/usr/bin/nectar-server nectar-server.service=/usr/lib/systemd/system/nectar-server.service ../genkeys.sh=/etc/nectar-server/genkeys.sh
 # Build RPM
-fpm -s dir -t rpm -a all -n nectar-server -v 0.2.1 --after-install nectar-server-install.sh ../target/Nectar-Server-0.2.1-SNAPSHOT.jar=/usr/lib/nectar-server/Nectar-Server.jar nectar-server=/usr/bin/nectar-server nectar-server.service=/usr/lib/systemd/system/nectar-server.service ../genkeys.sh=/etc/nectar-server/genkeys.sh
+fpm -s dir -t rpm -a all -n nectar-server -v 0.2.1 --iteration $BUILD_NUMBER --after-install nectar-server-install.sh ../target/Nectar-Server-0.2.1-SNAPSHOT.jar=/usr/lib/nectar-server/Nectar-Server.jar nectar-server=/usr/bin/nectar-server nectar-server.service=/usr/lib/systemd/system/nectar-server.service ../genkeys.sh=/etc/nectar-server/genkeys.sh
