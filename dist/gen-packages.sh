4cb79ad483e94807869f22467ac39b0920547225
jython234 <jython234@users.noreply.github.com>
2017-05-03 21:25:50 -0500

    Fix package building to actually give the correct version.

--- dist/gen-packages.sh
+++ dist/gen-packages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-NECTAR_SERVER_JAR=Nectar-Server-0.4.4-SNAPSHOT.jar
+NECTAR_VERSION=0.4.4
+NECTAR_SERVER_JAR=Nectar-Server-$NECTAR_VERSION-SNAPSHOT.jar
 BUILD_NUMBER=0
 
 if [ $# == 0 ]; then
@@ -10,6 +11,6 @@ else
 fi
 
 # Build Deb
-fpm -s dir -t deb -a all -n nectar-server -v 0.4.1 --iteration $BUILD_NUMBER --after-install nectar-server-install.sh ../target/$NECTAR_SERVER_JAR=/usr/lib/nectar-server/Nectar-Server.jar nectar-server=/usr/bin/nectar-server nectar-server.service=/usr/lib/systemd/system/nectar-server.service ../genkeys.sh=/etc/nectar-server/genkeys.sh
+fpm -s dir -t deb -a all -n nectar-server -v $NECTAR_VERSION --iteration $BUILD_NUMBER --after-install nectar-server-install.sh ../target/$NECTAR_SERVER_JAR=/usr/lib/nectar-server/Nectar-Server.jar nectar-server=/usr/bin/nectar-server nectar-server.service=/usr/lib/systemd/system/nectar-server.service ../genkeys.sh=/etc/nectar-server/genkeys.sh
 # Build RPM
-fpm -s dir -t rpm -a all -n nectar-server -v 0.4.1 --iteration $BUILD_NUMBER --after-install nectar-server-install.sh ../target/$NECTAR_SERVER_JAR=/usr/lib/nectar-server/Nectar-Server.jar nectar-server=/usr/bin/nectar-server nectar-server.service=/usr/lib/systemd/system/nectar-server.service ../genkeys.sh=/etc/nectar-server/genkeys.sh
+fpm -s dir -t rpm -a all -n nectar-server -v $NECTAR_VERSION --iteration $BUILD_NUMBER --after-install nectar-server-install.sh ../target/$NECTAR_SERVER_JAR=/usr/lib/nectar-server/Nectar-Server.jar nectar-server=/usr/bin/nectar-server nectar-server.service=/usr/lib/systemd/system/nectar-server.service ../genkeys.sh=/etc/nectar-server/genkeys.sh
